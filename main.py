086f2d66591169fcf6131d0a05f156f45ca7319c
Seho Lim <limseho657424@gmail.com>
2017-02-13 13:30:33 -0600

    deleted requests moduel. Added post method in the helper function for retrieving google token.

--- main.py
+++ main.py
@@ -7,11 +7,6 @@ from flask import session as login_session
 import random
 import string
 
-from oauth2client.client import flow_from_clientsecrets
-from oauth2client.client import FlowExchangeError
-# Create a service object
-from apiclient import discovery
-
 
 # HTTP client library
 import httplib2
@@ -21,108 +16,76 @@ import json
 # Models
 from models.Course import Course
 from models.Card import Card
+from oauth2client import client, crypt
 
 app = Flask(__name__)
 app.secret_key = 'super_secret_key'
 
 CLIENT_ID = json.loads(
     open('client_secrets.json', 'r').read())['web']['client_id']
-APPLICATION_NAME = "flashCardApp"
 
 
 # Create  anti forgery state token
 @app.route('/login')
-def showLogin():
-    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
-                    for x in xrange(32))
-    login_session['state'] = state
-    # return "The current session state is %s" % login_session['state']
-    return render_template('login.html', STATE=state)
+def showLogin():    
+    return render_template('login.html')
 
 
-@app.route('/gconnect', methods=['POST'])
-def gconnect():
-    # Validate state token
-    if request.args.get('state') != login_session['state']:
-        response = make_response(json.dumps('Invalid state parameter.'), 401)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-    # Obtain authorization code
-    code = request.data
+@app.route('/tokensignin', methods=['POST'])
+def connect():
     try:
-        # Upgrade the authorization code into a credentials object
-        flow = flow_from_clientsecrets('client_secrets.json', scope='')
-        flow.redirect_uri = 'postmessage'
-        credentials = flow.step2_exchange(code)
-    except FlowExchangeError:
-        response = make_response(
-            json.dumps('Failed to upgrade the authorization code.'), 401)
-        response.headers['Content-Type'] = 'application/json'
-        return response
+    idinfo = client.verify_id_token(token, CLIENT_ID)
+
+    # Or, if multiple clients access the backend server:
+    #idinfo = client.verify_id_token(token, None)
+    #if idinfo['aud'] not in [CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]:
+    #    raise crypt.AppIdentityError("Unrecognized client.")
+
+    if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+        raise crypt.AppIdentityError("Wrong issuer.")
+
+    # If auth request is from a G Suite domain:
+    #if idinfo['hd'] != GSUITE_DOMAIN_NAME:
+    #    raise crypt.AppIdentityError("Wrong hosted domain.")
+    except crypt.AppIdentityError:
+    # Invalid token
+    userid = idinfo['sub']
+    
+
+    # (Receive token by HTTPS POST)
+
+    try:
+        idinfo = client.verify_id_token(token, CLIENT_ID)
+
+        # Or, if multiple clients access the backend server:
+        #idinfo = client.verify_id_token(token, None)
+        #if idinfo['aud'] not in [CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]:
+        #    raise crypt.AppIdentityError("Unrecognized client.")
+
+        if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+            raise crypt.AppIdentityError("Wrong issuer.")
+
+        # If auth request is from a G Suite domain:
+        #if idinfo['hd'] != GSUITE_DOMAIN_NAME:
+        #    raise crypt.AppIdentityError("Wrong hosted domain.")
+    except crypt.AppIdentityError:
+        # Invalid token
+        print "invalid token"
+    userid = idinfo['sub']
+
+    return render_template('login.html', userid=userid)
 
 
-    # Check that the access token is valid.
-    access_token = credentials.access_token
-    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
-           % access_token)
-    h = httplib2.Http()
-    result = json.loads(h.request(url, 'GET')[1])
-    # If there was an error in the access token info, abort.
-    if result.get('error') is not None:
-        response = make_response(json.dumps(result.get('error')), 500)
-        response.headers['Content-Type'] = 'application/json'
-        return response
 
-    # Verify that the access token is used for the intended user.
-    gplus_id = credentials.id_token['sub']
-    if result['user_id'] != gplus_id:
-        response = make_response(
-        json.dumps("Token's user ID doesn't match given user ID."), 401)
-        response.headers['Content-Type'] = 'application/json'
-        return response
 
-    # Verify that the access token is valid for this app.
-    if result['issued_to'] != CLIENT_ID:
-        response = make_response(
-            json.dumps("Token's client ID does not match app's."), 401)
-        print "Token's client ID does not match app's."
-        response.headers['Content-Type'] = 'application/json'
-        return response
 
-    stored_credentials = login_session.get('credentials')
-    stored_gplus_id = login_session.get('gplus_id')
-    if stored_credentials is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps('Current user is already connected.'),
-                                 200)
-        response.headers['Content-Type'] = 'application/json'
-        return response
 
-    # Store the access token in the session for later use.
-    login_session['credentials'] = credentials
-    login_session['gplus_id'] = gplus_id
+# Render courses page.
+@app.route('/courses')
+def my_courses():
+    courses = db.GqlQuery("SELECT * FROM Course ORDER BY created")
+    return render_template('myCourses.html', courses=courses)
 
-    # Get user info
-
-    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
-    params = {'access_token': credentials.access_token, 'alt': 'json'}
-    answer = requests.get(userinfo_url, params=params)
-
-    data = answer.json()
-
-    login_session['username'] = data['name']
-    login_session['picture'] = data['picture']
-    login_session['email'] = data['email']
-
-    output = ''
-    output += '<h1>Welcome, '
-    output += login_session['username']
-    output += '!</h1>'
-    output += '<img src="'
-    output += login_session['picture']
-    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
-
-    print "done!"
-    return output
 
 @app.route('/courses/new', methods=['GET', 'POST'])
 def new_course():
@@ -134,14 +97,7 @@ def new_course():
             c.put()
             return redirect(url_for('my_courses'))
     else:
-        return render_template('newCourse.html')
-
-
-# Render courses page.
-@app.route('/courses')
-def my_courses():
-    courses = db.GqlQuery("SELECT * FROM Course ORDER BY created")
-    return render_template('myCourses.html', courses=courses)
+        return render_template('newCourse.html')    
 
 
 # See the course's cards.
